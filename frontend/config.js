caf72527cbd616af8ca9e6a0dc9189307d37b529
refact0r <34758569+refact0r@users.noreply.github.com>
2026-02-08 05:10:26 -0800

    test build

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,2 @@
-const meta = document.querySelector('meta[name="api-base-url"]')
-
 export const API_BASE_URL =
-	(window.API_BASE_URL && String(window.API_BASE_URL)) ||
-	(meta && meta.content) ||
-	'http://localhost:5001'
+	'https://uncondemnable-disorganizedly-nerissa.ngrok-free.dev'
